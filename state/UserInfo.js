4859189820d58c0f933179d10b78c1ac7022d5c0
Genesis Garcia <genesisg334@Gmail.com>
2020-05-14 16:43:59 -0700

    camera overlay

--- state/UserInfo.js
+++ state/UserInfo.js
@@ -1,8 +1,8 @@
 // Later this file will get this information from the email +
 // password unlocking the user's information
-const user_id = 4;
-const child_id = 6;
-const parent_id = 2;
+const user_id = 1;
+const child_id = 1;
+const parent_id = 1;
 const pincode = "1234";
 
 function getUserInfo(env) {
