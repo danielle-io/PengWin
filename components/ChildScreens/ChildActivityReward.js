cd318f2ddbbf0421986a55d84c83af755aba94c6
archi59 <architag@gmail.com>
2020-05-21 20:56:51 -0700

    B4 merge

---
+++ components/ChildScreens/ChildActivityReward.js
@@ -0,0 +1,236 @@
+import React, { Component } from "react";
+import { Dimensions, Text, View, TouchableOpacity, Image } from "react-native";
+
+import { Video } from "expo-av";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+Icon.loadFont();
+
+import Star from "../../assets/images/Star.png";
+import StarFill from "../../assets/images/fillstar.png";
+
+import Environment from "../../database/sqlEnv";
+import UserInfo from "../../state/UserInfo";
+import { AppLoading } from "expo";
+import { ScrollView } from "react-native-gesture-handler";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+export default class ChildActivityReward extends Component {
+  //Header titles for routine notif
+  static navigationOptions = ({ navigation }) => ({
+    title: "Routines",
+    headerRight: () => (
+      <Icon
+        style={{ padding: 15, color: "#848484" }}
+        name={"account-circle"}
+        size={25}
+        onPress={() => {
+          navigation.navigate("ChildPincode", {
+            prevScreenTitle: "My Routines",
+          });
+        }}
+      />
+    ),
+  });
+
+  constructor(props) {
+    super(props);
+    const { navigate } = this.props.navigation;
+    this.navigate = navigate;
+    this.state = {
+      activityId: this.props.navigation.state.params.activityId,
+      length: this.props.navigation.state.params.length,
+      key: this.props.navigation.state.params.key + 1,
+      activity: null,
+      loaded: false,
+    };
+    this.getActivity();
+  }
+
+  getActivity() {
+    const parentId = UserInfo.parent_id;
+    const childId = UserInfo.child_id;
+    const userId = UserInfo.user_id;
+
+    fetch(Environment + "/getActivityById/" + this.state.activityId, {
+      headers: {
+        "Cache-Control": "no-cache",
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson;
+      })
+      .then((results) => {
+        results.map((item) => {
+          this.setState({ activity: item });
+          this.setState({ loaded: true });
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  renderStars() {
+    let star = [];
+
+    for (let i = 0; i < this.state.length; i++) {
+      if (i < this.state.key) {
+        star.push(<Image source={StarFill} style={{ margin: 10 }} />);
+      } else {
+        star.push(<Image source={Star} style={{ margin: 10 }} />);
+      }
+    }
+
+    return star;
+  }
+
+  renderText() {
+    if (this.state.key === this.state.length) {
+      return (
+        <Text style={styles.title}>Wow you just completed your routine!!</Text>
+      );
+    } else if (this.state.key == 1) {
+      return (
+        <Text style={styles.title}>
+          You got a star for completing your activity!
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.title}>
+          You got {this.state.key} stars for completing your activity!
+        </Text>
+      );
+    }
+  }
+
+  renderButton() {
+    if (this.state.key === this.state.length) {
+      return <Text style={styles.textStyle}>What's My Reward?</Text>;
+    } else {
+      return <Text style={styles.textStyle}>Next Task!</Text>;
+    }
+  }
+
+  render() {
+    if (this.state.loaded) {
+      return (
+        <ScrollView>
+          <View style={styles.container}>
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: 100,
+              }}
+            >
+              <View style={styles.image}>{this.renderStars()}</View>
+              {this.renderText()}
+            </View>
+            {this.state.activity.reward_image && (
+              <View
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={{ uri: this.state.activity.reward_image }}
+                  style={{
+                    width: WIDTH * 0.6,
+                    height: WIDTH * 0.3,
+                    marginBottom: 50,
+                    borderRadius: 15,
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+            )}
+
+            {this.state.activity.reward_video && (
+              <View>
+                <View
+                  style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Video
+                    useNativeControls={true}
+                    source={{ uri: this.state.activity.reward_video }}
+                    rate={1.0}
+                    volume={1.0}
+                    isMuted={false}
+                    resizeMode="contain"
+                    isLooping
+                    style={{ width: WIDTH * 0.7, height: WIDTH * 0.3 }}
+                  />
+                </View>
+              </View>
+            )}
+
+            {this.state.activity.reward_description && (
+              <View>
+                <Text style={styles.section}>
+                  {this.state.activity.reward_description}
+                </Text>
+              </View>
+            )}
+
+            <TouchableOpacity
+              style={styles.buttonStyle}
+              onPress={() => {
+                this.navigate("ChildActivity", {});
+              }}
+            >
+              {this.renderButton()}
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      );
+    } else {
+      return <AppLoading />;
+    }
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    height: HEIGHT,
+
+    top: -20,
+  },
+  image: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    margin: 30,
+  },
+  section: {
+    fontSize: 25,
+    alignItems: "center",
+    margin: 30,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  textStyle: {
+    fontSize: 20,
+    color: "white",
+    textAlign: "center",
+  },
+  buttonStyle: {
+    width: 300,
+    padding: 10,
+    margin: 20,
+    backgroundColor: "#B1EDE8",
+    borderRadius: 100,
+  },
+};
---
+++ components/ParentSide/Questionnaire/Question4.js
@@ -0,0 +1,230 @@
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+// import MobileStepper from "@bit/mui-org.material-ui.mobile-stepper";
+import StepIndicator from 'react-native-step-indicator';
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+let customFonts = {
+    'SF': require("../../../assets/fonts/SF/SF-Pro-Rounded-Medium.otf")
+    // 'SF-Pro-Rounded-Semibold': require("../../../assets/fonts/SF/SF-Pro-Rounded-Semibold.otf"),
+    // 'SF-Pro-Rounded-Regular': require("../../../assets/fonts/SF/SF-Pro-Rounded-Regular.otf")
+}
+
+var radio_props = [
+    {label: 'Verbal', value: 0 },
+    {label: 'Non-verbal but can say Yes/No', value: 1 },
+    {label: 'Can speak but not everyone understands', value: 2 },
+    {label: 'Cannot speak but knows words', value: 3 },
+    {label: 'Non verbal', value: 4 }
+  ];
+
+export default class Question2 extends Component {
+    constructor(props) {
+        super(props);
+        const { navigate } = this.props.navigation;
+        this.navigate = navigate;
+        this.state = {
+          fontsLoaded: false,
+          currentPosition: 3,
+          selected: false
+        //   value: null,
+        };
+      }
+
+
+    static navigationOptions = ({ navigation }) => ({
+    title: 'Questionnaire',
+    prevScreenTitle: 'Back'
+    });
+
+    async _loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+      }
+
+    componentDidMount() {
+    this._loadFontsAsync();
+    }
+    
+    render () {
+        // if (this.state.fontsLoaded) {
+            return (
+                // console.log("radio button options", radio_props[1].value),
+
+                <View style={{backgroundColor:"#FFFCF9"}}>
+                    <Text style={styles.pageHeader}>Create your child's profile</Text>
+
+                <StepIndicator
+                    customStyles={customStyles}
+                    stepCount= {4}
+                    currentPosition={this.state.currentPosition}
+                    // labels={labels}
+                />
+
+                <View style={styles.pageBodyContainer}>
+                <Text style={styles.pageBodyText}>4. How would you describe your child’s language ability?</Text>
+
+                    {/* <View style={styles.radioButtonsContainer}> */}
+
+                    <View style={styles.radioButtons}>
+
+                        <RadioForm 
+                        radio_props={radio_props}
+                        initial={0}
+                        buttonColor={'#352D39'}
+                        labelStyle={{margin: 8, fontSize: 22, color: '#352D39'}}
+                        // style= {{lineHeight: 5}}
+                        labelWrapStyle ={{lineHeight: 5}}
+                        labelColor={'#352D39'}
+                        selectedButtonColor={'#352D39'}
+                        onPress={(choice) => {this.setState({value:choice}); this.state.selected = true} }
+                        //it shows the value of previously selected choice. don't know why¯\_(ツ)_/¯
+                        />
+                    </View>
+                </View>
+                    
+
+                    {/* </View> */}
+                    
+                <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                style={this.state.selected
+                        ? styles.buttonPrimary
+                        : styles.buttonSecondary}
+                onPress={() => {this.navigate("ParentRoutines",{prevScreenTitle: 'Back'}); console.log("valueee", this.state.value); console.log("clicked")}}
+                >
+                <View>
+                <Text style={this.state.selected
+                        ? styles.buttonPrimaryText
+                        : styles.buttonSecondaryText}>Continue</Text>
+                </View>
+                
+                
+                </TouchableOpacity>
+                </View>
+                </View>
+
+            )
+
+        // }
+        
+    }
+}
+
+const pastelRed = '#FF6978';
+const spaceBlack = '#352D39';
+
+const styles = StyleSheet.create({
+    pageHeader: {
+        // fontFamily: 'SF',
+        fontSize: 32,
+        color: spaceBlack,
+        textAlign: 'center',
+        margin: 30,
+        textTransform: 'uppercase'
+    },
+
+    pageBodyText: {
+        // fontFamily: 'SF-Pro-Rounded-Medium',
+        color: spaceBlack,
+        fontSize: 24,
+        textAlign: 'center',
+    },
+
+    // radioButtonsContainer: {
+    //     alignContent: 'center',
+    //     display: "flex",
+    //     justifyContent: 'center',
+        
+    // },
+
+    pageBodyContainer: {
+        marginTop: '15%'
+    },
+    
+    radioButtons: {
+        // fontFamily: 'SF-Pro-Rounded-Regular',
+        fontSize: 24,
+        textAlign: 'center',
+        marginLeft: '25%',
+        marginTop: '5%'
+    },
+
+    buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: "flex",
+        marginBottom: 800
+    },
+
+    buttonSecondary: {
+        alignContent: 'center',
+        borderRadius: 30,
+        width: 290,
+        height: 60,
+        paddingVertical: 10,
+        paddingHorizontal: 76,
+        borderWidth: 2,
+        borderColor: pastelRed,
+        backgroundColor: 'white',
+        marginTop: '40%'
+    },
+    
+      buttonPrimary: {
+        backgroundColor: pastelRed,
+        borderRadius: 30,
+        width: 290,
+        height: 60,
+        paddingVertical: 10,
+        paddingHorizontal: 76,
+        borderWidth: 2,
+        borderColor: pastelRed,
+        alignContent: 'center',
+        marginTop: '40%'
+      },
+    
+      buttonSecondaryText: {
+        // fontFamily: 'SF-Pro-Rounded-Semibold',
+        fontSize: 30,
+        fontWeight: "600",
+        textAlign: 'center',
+        color: pastelRed
+      },
+    
+      buttonPrimaryText: {
+        // fontFamily: 'SF-Pro-Rounded-Semibold',
+        fontSize: 30,
+        fontWeight: "600",
+        textAlign: 'center',
+        color: 'white'
+      }
+    
+})
+
+const customStyles = {
+    stepIndicatorSize: 20,
+    currentStepIndicatorSize:30,
+    separatorStrokeWidth: 0,
+    currentStepStrokeWidth: 2,
+    stepStrokeCurrentColor: '#FF6978',
+    stepStrokeWidth: 1,
+    stepStrokeFinishedColor: '#FF6978',
+    stepStrokeUnFinishedColor: '#C4C4C4',
+    // separatorFinishedColor: '#fe7013',
+    // separatorUnFinishedColor: '#aaaaaa',
+    stepIndicatorFinishedColor: '#FF6978',
+    stepIndicatorUnFinishedColor: '#C4C4C4',
+    stepIndicatorCurrentColor: '#FF6978',
+    // stepIndicatorLabelFontSize: 13,
+    // currentStepIndicatorLabelFontSize: 13,
+    stepIndicatorLabelCurrentColor: '#FF6978',
+    stepIndicatorLabelFinishedColor: '#FF6978',
+    stepIndicatorLabelUnFinishedColor: '#C4C4C4',
+    labelColor: '#999999',
+    margin: '50%'
+    // labelSize: 13,
+    // currentStepLabelColor: '#fe7013'
+  }
